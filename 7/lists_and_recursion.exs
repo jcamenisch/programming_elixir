625201c3a1a1b2233cef355336312e5e7fbd6646
Jonathan Camenisch <jonathan@camenisch.net>
2016-01-12 08:59:34 -0500

    More chapter 7

--- 7/lists_and_recursion.exs
+++ 7/lists_and_recursion.exs
@@ -1,17 +1,33 @@
 defmodule ListsAndRecursion do
-  def mapsum([], _func) do
-    0
+  def sum([]), do: 0
+
+  def sum([head | tail]) do
+    head + sum(tail)
   end
+
+
+  def mapsum([], _func), do: 0
 
   def mapsum([head | tail], func) do
     _mapsum(tail, func.(head), func)
   end
 
-  defp _mapsum([], value, _func) do
-    value
-  end
+  defp _mapsum([], value, _func), do: value
 
   defp _mapsum([head | tail], value, func) do
     _mapsum(tail, value + func.(head), func)
+  end
+
+  # Just for fun while I'm thinking about it
+  def join(_list, sep \\ " ")
+
+  def join([], _), do: ""
+
+  def join([head | []], _) do
+    head
+  end
+
+  def join([head | tail], sep) do
+    head <> sep <> join(tail)
   end
 end
